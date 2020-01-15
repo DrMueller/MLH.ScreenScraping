37181fb1a3b3b7fc985d24d12554bb12f37f92b0
Matthias Müller <matthiasm@live.de>
2020-01-15 21:29:44 +0100

    WIP

--- Sources/NugetTestUI/MainWindow.xaml.cs
+++ Sources/NugetTestUI/MainWindow.xaml.cs
@@ -21,22 +21,7 @@ namespace Mmu.Mlh.ScreenScraping.NugetTestUI
             await browser.Navigate("https://www.google.ch");
 
             browser
-                .Find<InputElement>(f => f.ById("UsernameOrEmail"))
-                .SetValue("SomeValue");
-
-            browser
-                .Find<InputElement>(f => f.ById("Password"))
-                .SetValue("SomeValue");
-
-            browser
-                .Find<WebElement>(f => f.ByClassName("btn btn-primary"))
-                .Click();
-
-            await browser.WaitForPageToLoad("https://www.google.ch");
-
-            browser
-                .FindAll(f => f.ByInnerText("Benutzerkonten"))
-                .Single(f => f.TagName.ToUpperInvariant() == "BUTTON")
+                .Find<WebElement>(f => f.ByClassName("gLFyf gsfi"))
                 .Click();
         }
     }
